1dec688ca2021b61a11192f9198bef50249cedc4
vishnukarthik29 <vishnukarthik2912@gmail.com>
2025-12-05 21:39:43 +0530

    updated the currency dropdown to show the country details with currency symbol

--- src/components/CurrencyDropdown.vue
+++ src/components/CurrencyDropdown.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="w-full">
     <label class="block text-gray-600 mb-2">{{ label }}</label>
+
     <select
       v-model="selectedValue"
       class="w-full border border-gray-300 rounded-lg p-2 focus:ring focus:ring-blue-200 focus:outline-none"
     >
       <option v-for="cur in currencies" :key="cur" :value="cur">
-        {{ cur }}
+        {{ formatCurrency(cur) }}
       </option>
     </select>
   </div>
@@ -14,6 +15,7 @@
 
 <script setup>
 import { computed } from "vue";
+import { currencyInfo } from "@/data/CurrencyInfo.js";
 
 const props = defineProps({
   label: String,
@@ -27,4 +29,10 @@ const selectedValue = computed({
   get: () => props.modelValue,
   set: (val) => emit("update:modelValue", val),
 });
+
+const formatCurrency = (code) => {
+  const info = currencyInfo[code];
+  if (!info) return code;
+  return `${info.country} (${info.symbol}) - ${code}`;
+};
 </script>
